e9f71d4eb36b14248a1268a168c231d7024b2d02
Emily Sellers <emilymelindasellers@gmail.com>
2022-12-06 11:49:33 -0800

    add test for /callback should login users and redirect to dashboard, passing

--- __tests__/github.test.js
+++ __tests__/github.test.js
@@ -16,8 +16,24 @@ describe('github auth', () => {
 
   it('/api/v1/github/login should redirect to the github oauth page', async () => {
     const res = await request(app).get('/api/v1/github/login');
+    console.log(res.header.location);
     expect(res.header.location).toMatch(
       /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/callback/i
     );
+  });
+
+  it('/api/v1/github/callback should login users and redirect to dashboard', async () => {
+    const res = await request
+      .agent(app)
+      .get('/api/v1/github/callback?code=17')
+      .redirects(1);
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      login: 'mock-github-user',
+      email: 'mock@test.com',
+      avatar: 'https://www.placecage.com/gif/300/300',
+      iat: expect.any(Number),
+      exp: expect.any(Number),
+    });
   });
 });
